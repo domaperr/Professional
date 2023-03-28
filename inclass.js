b763c899f3116a6608b8feb775e9f1ed92444ba6
domaperr <123401813+domaperr@users.noreply.github.com>
2023-03-28 15:47:13 -0500

    Add files via upload

---
+++ inclass.js
@@ -0,0 +1,57 @@
+$.validator.setDefaults({
+    submitHandler: function() {
+        alert("submitted!");
+    }
+});
+
+$().ready(function() {
+    // validate the comment form when it is submitted
+    $("#commentForm").validate();
+
+    // validate signup form on keyup and submit
+    $("#signupForm").validate({
+        rules: {
+            firstname: "required",
+            lastname: "required",
+            username: {
+                required: true,
+                minlength: 8
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            agree: "required"
+        },
+        messages: {
+            firstname: "Please enter your firstname",
+            lastname: "Please enter your lastname",
+            username: {
+                required: "Please enter a username",
+                minlength: "Your username must consist of at least 2 characters"
+            },
+            email: "Please enter a valid email address",
+        }
+    });
+
+    // propose username by combining first- and lastname
+    $("#username").focus(function() {
+        var firstname = $("#firstname").val();
+        var lastname = $("#lastname").val();
+        if (firstname && lastname && !this.value) {
+            this.value = firstname + "." + lastname;
+        }
+    });
+
+    //code to hide topic selection, disable for demo
+    var newsletter = $("#newsletter");
+    // newsletter topics are optional, hide at first
+    var inital = newsletter.is(":checked");
+    var topics = $("#newsletter_topics")[inital ? "removeClass" : "addClass"]("gray");
+    var topicInputs = topics.find("input").attr("disabled", !inital);
+    // show when newsletter is checked
+    newsletter.click(function() {
+        topics[this.checked ? "removeClass" : "addClass"]("gray");
+        topicInputs.attr("disabled", !this.checked);
+    });
+});
